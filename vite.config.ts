36c0ef0eb418e21f63d60c740307dec94c17652d
seran <50622459+serandev@users.noreply.github.com>
2025-06-01 15:42:19 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/sb1-5tb6c3gu/'
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
