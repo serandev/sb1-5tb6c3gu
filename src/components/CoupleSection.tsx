a4cd01b6e7c758617da8f1c25c960fb0c9dd4c07
serandev <chocolate1037@gmail.com>
2025-06-01 21:02:10 +0900

    main image 조정

--- src/components/CoupleSection.tsx
+++ src/components/CoupleSection.tsx
@@ -36,7 +36,7 @@ const CoupleSection: React.FC = () => {
         backgroundPosition: 'center 20%',
       }}
     >
-      <div className="container mx-auto mt-10 px-4 py-8 bg-background/70 backdrop-blur-sm">
+      <div className="container mx-auto mt-10 px-4 py-2 bg-background/70 backdrop-blur-sm">
         <div className="max-w-3xl mx-auto text-center">
           {/* <p className="text-accent font-light mb-4 tracking-widest"> */}
             {/* <strong>저희 결혼합니다</strong> */}
