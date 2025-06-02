dd82303336f412eb8c8d163e847f6aeb0e9c00d7
seren.a <seren.a@kakaocorp.com>
2025-06-02 20:07:45 +0900

    상단 미세조정

--- src/components/CoupleSection.tsx
+++ src/components/CoupleSection.tsx
@@ -36,7 +36,7 @@ const CoupleSection: React.FC = () => {
             backgroundPosition: 'center 20%',
           }}
       >
-        <div className="absolute inset-0 flex items-start justify-center pt-9">
+        <div className="absolute inset-0 flex items-start justify-center pt-8">
           <div className="container mx-auto px-4 py-2 bg-background/70 backdrop-blur-sm">
             <div className="max-w-3xl mx-auto text-center">
               <h1
